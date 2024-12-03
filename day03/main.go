7a9c70a1b7d0c82e8bd65d20d9196a70d55b0888
Kirill Tishenkov <solokirrik@gmail.com>
2024-12-03 21:29:43 +0100

    add day03 p2

--- day03/main.go
+++ day03/main.go
@@ -10,44 +10,61 @@ import (
 	"github.com/samber/lo"
 )
 
-//go:embed ex
+//go:embed inp
 var inp string
 
 func main() {
-	inplines := strings.Split(inp, "\n")
-
-	slog.Info("Part 1:", "safe:", part1(inplines))
-	slog.Info("Part 2:", "safe:", part2(inplines))
+	slog.Info("Part 1:", "Mul:", part1(inp))
+	slog.Info("Part 2:", "Mul:", part2(inp))
 }
 
-func part1(inp []string) int64 {
-	result := int64(0)
-	for _, row := range inp {
-		result += calcMulsRow(row)
+var (
+	mulRex = regexp.MustCompile("mul\\(\\d{1,3},\\d{1,3}\\)")
+	doRex  = regexp.MustCompile("do\\(\\)|don't\\(\\)")
+)
+
+func part1(row string) int {
+	return calcMulsRow(row)
+}
+
+func part2(row string) int {
+	indexes := lo.Map(doRex.FindAllStringIndex(row, -1), func(startEndPair []int, _ int) int { return startEndPair[0] })
+
+	if indexes[0] != 0 {
+		indexes = append([]int{0}, indexes...)
 	}
 
-	return result
-}
+	indexes = append(indexes, len(row))
 
-var rego = regexp.MustCompile("mul\\(\\d{1,3},\\d{1,3}\\)")
+	sum := 0
 
-func calcMulsRow(row string) int64 {
-	pairs := lo.Map(rego.FindAllStringSubmatch(row, -1), func(item []string, _ int) []int64 {
-		vals := strings.Split(item[0][4:len(item[0])-1], ",")
-		n1, _ := strconv.ParseInt(vals[0], 10, 64)
-		n2, _ := strconv.ParseInt(vals[1], 10, 64)
-		return []int64{n1, n2}
-	})
+	for i := 1; i < len(indexes); i++ {
+		l, r := indexes[i-1], indexes[i]
+		substr := row[l:r]
 
-	mults := lo.Map(pairs, func(inner []int64, _ int) int64 {
-		return lo.Reduce(inner, func(acc, item int64, _ int) int64 { return acc * item }, 1)
-	})
+		if strings.HasPrefix(substr, "don't()") {
+			continue
+		}
 
-	sum := lo.Reduce(mults, func(acc, item int64, _ int) int64 { return acc + item }, 0)
+		sum += calcMulsRow(substr)
+	}
 
 	return sum
 }
 
-func part2(_ []string) int {
-	return 0
+func calcMulsRow(row string) int {
+	pairs := lo.Map(mulRex.FindAllStringSubmatch(row, -1), func(item []string, _ int) []int {
+		vals := strings.Split(item[0][4:len(item[0])-1], ",")
+		n1, _ := strconv.ParseInt(vals[0], 10, 64)
+		n2, _ := strconv.ParseInt(vals[1], 10, 64)
+		return []int{int(n1), int(n2)}
+	})
+
+	mults := lo.Map(pairs, func(inner []int, _ int) int {
+		return lo.Reduce(inner, func(acc, item int, _ int) int { return acc * item }, 1)
+	})
+
+	sum := lo.Reduce(mults, func(acc, item int, _ int) int { return acc + item }, 0)
+
+	return sum
 }
--- day03/main_test.go
+++ day03/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "embed"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,15 +10,18 @@ import (
 //go:embed ex
 var testEx string
 
+//go:embed ex2
+var testEx2 string
+
 //go:embed inp
 var testInp string
 
 func Test_Part1(t *testing.T) {
-	assert.Equal(t, int64(2*4+5*5+11*8+8*5), part1(strings.Split(testEx, "\n")))
-	assert.Equal(t, int64(192767529), part1(strings.Split(testInp, "\n")))
+	assert.Equal(t, 2*4+5*5+11*8+8*5, part1(testEx))
+	assert.Equal(t, 192767529, part1(testInp))
 }
 
 func Test_Part2(t *testing.T) {
-	assert.Equal(t, 0, part2(strings.Split(testEx, "\n")))
-	assert.Equal(t, 0, part2(strings.Split(testInp, "\n")))
+	assert.Equal(t, 2*4+8*5, part2(testEx2))
+	assert.Equal(t, 104083373, part2(testInp))
 }
