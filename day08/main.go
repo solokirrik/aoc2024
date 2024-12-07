4cdf8bab9083c7c5fb37a479ed6a3fa6595fa3ff
Kirill Tishenkov <solokirrik@gmail.com>
2024-12-07 21:23:05 +0100

    init day08

---
+++ day08/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	_ "embed"
+	"log/slog"
+)
+
+//go:embed inp
+var inp string
+
+func main() {
+	slog.Info("Part 1:", "Ans:", new(solver).prep(inp).part1())
+	slog.Info("Part 2:", "Ans:", new(solver).prep(inp).part2())
+}
+
+type solver struct{}
+
+func (s *solver) prep(_ string) *solver { return s }
+
+func (s *solver) part1() int { return 0 }
+func (s *solver) part2() int { return 0 }
