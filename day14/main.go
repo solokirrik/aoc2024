40670461a3c7cf448d5966f3ac11df553379164a
Kirill Tishenkov <solokirrik@gmail.com>
2024-12-14 08:13:31 +0100

    add day14 parsing

--- day14/main.go
+++ day14/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"log/slog"
+	"strconv"
 	"strings"
 )
 
@@ -10,23 +11,67 @@ import (
 var inp string
 
 func main() {
-	slog.Info("Part 1:", "Ans:", new(solver).prep(inp).part1())
-	slog.Info("Part 2:", "Ans:", new(solver).prep(inp).part2())
+	slog.Info("Part 1:", "Ans:", new(solver).prep(inp, 101, 103).part1(100))
+	slog.Info("Part 2:", "Ans:", new(solver).prep(inp, 101, 103).part2(100))
 }
 
 type solver struct {
+	robots []robot
+	fs     coor
 }
 
-func (s *solver) prep(inp string) *solver {
-	_ = strings.Split(inp, "\n")
+func (s *solver) prep(inp string, fieldW, fieldH int64) *solver {
+	for _, line := range strings.Split(inp, "\n") {
+		s.robots = append(s.robots, newRobot(line))
+	}
+
+	s.fs = coor{fieldH, fieldH}
 
 	return s
 }
 
-func (s *solver) part1() int {
-	return 0
+func (s *solver) part1(wait int64) int {
+	sum := 0
+
+	for i := range s.robots {
+		s.robots[i].move(wait)
+	}
+
+	return sum
 }
 
-func (s *solver) part2() int {
-	return 0
+func (s *solver) part2(wait int64) int {
+	sum := 0
+	return sum
+}
+
+type robot struct {
+	pos coor
+	vel coor
+}
+
+func (r *robot) move(n int64) *robot {
+
+	return r
+}
+
+func newRobot(in string) robot {
+	p, v, _ := strings.Cut(in, " ")
+	_, p, _ = strings.Cut(p, "=")
+	_, v, _ = strings.Cut(v, "=")
+
+	return robot{
+		pos: parsePair(p),
+		vel: parsePair(p),
+	}
+}
+
+func parsePair(in string) coor {
+	x, _ := strconv.ParseInt(strings.Split(in, ",")[0], 10, 64)
+	y, _ := strconv.ParseInt(strings.Split(in, ",")[1], 10, 64)
+	return coor{x: x, y: y}
+}
+
+type coor struct {
+	x, y int64
 }
--- day14/main_test.go
+++ day14/main_test.go
@@ -14,11 +14,31 @@ var testEx string
 var testInp string
 
 func Test_Part1(t *testing.T) {
-	assert.Equal(t, 0, new(solver).prep(testEx).part1())
-	assert.Equal(t, 0, new(solver).prep(testInp).part1())
+	t.Run("ex0", func(t *testing.T) {
+		s := solver{robots: []robot{{pos: coor{x: 2, y: 4}, vel: coor{x: 2, y: -3}}}}
+		r := s.robots[0].move(5)
+		assert.Equal(t, robot{pos: coor{x: 1, y: 3}, vel: coor{x: 2, y: -3}}, r)
+	})
+
+	t.Run("ex1", func(t *testing.T) {
+		s := solver{robots: []robot{{pos: coor{x: 2, y: 4}, vel: coor{x: 2, y: -3}}}}
+		r := s.robots[0].move(2)
+		assert.Equal(t, robot{pos: coor{x: 1, y: 3}, vel: coor{x: 2, y: -3}}, r)
+	})
+
+	t.Run("ex", func(t *testing.T) {
+		assert.Equal(t, 12, new(solver).prep(testEx, 10, 7).part1(100))
+	})
+	t.Run("1", func(t *testing.T) {
+		assert.Equal(t, 0, new(solver).prep(testInp, 101, 103).part1(100))
+	})
 }
 
 func Test_Part2(t *testing.T) {
-	assert.Equal(t, 0, new(solver).prep(testEx).part2())
-	assert.Equal(t, 0, new(solver).prep(testInp).part2())
+	t.Run("ex", func(t *testing.T) {
+		assert.Equal(t, 0, new(solver).prep(testEx, 10, 7).part2(100))
+	})
+	t.Run("2", func(t *testing.T) {
+		assert.Equal(t, 0, new(solver).prep(testInp, 101, 103).part2(100))
+	})
 }
